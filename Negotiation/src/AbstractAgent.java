8aad0a10d5874426823605885c1f2484a45aaa2b
Erik kvanli <0erikkvanli@gmail.com>
2013-03-17 16:23:00 +0100

    ..

--- Negotiation/src/AbstractAgent.java
+++ Negotiation/src/AbstractAgent.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
+
 import jade.core.Agent;
 
 
 public class AbstractAgent extends Agent{
-
+	public static final int startingMoney = 10000;
+	protected int money;
+	protected ArrayList<Item> owndItems;
+	protected ArrayList<Item> wantedItems;
 	
+	public AbstractAgent(){
+		this.money = startingMoney;
+		owndItems = new ArrayList<>();
+		wantedItems = new ArrayList<>();
+		
+	}
 	
+	public void giveItem(Item item){
+		this.owndItems.add(item);
+	}
+	public void wantThisItem(Item item){
+		this.wantedItems.add(item);
+	}
 	
 	@Override
 	protected void setup() {
-		
+		//start here.
 	}
 }
---
+++ Negotiation/src/ControllerClass.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class ControllerClass {
+	public static void main(String[] args) {
+		ArrayList<Item> itemList = Item.createItems();
+		
+	}
+}
